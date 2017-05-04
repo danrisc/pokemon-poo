f308138c5f7559697d57113b08f8f98a2f43cac7
daniela risco <driscop@gmail.com>
2017-05-04 11:08:41 -0300

    subiendo ejercicio guiado-poo

---
+++ main.js
@@ -0,0 +1,23 @@
+function Pokemon(nombre,color,poderDeAtaque){
+	this.nombre= nombre;
+	this.color= color;
+	this.nivelDeAmistad = 0 
+	this.vida = 100
+	this.poderDeAtaque= poderDeAtaque
+
+	this.mostrarPokemon = function(){
+		return("hola, soy: "+ this.nombre+ "y soy de color"+ this.color)
+	}
+    this.aumentarAmistad = function(valor){
+    	this.nivelDeAmistad = this.nivelDeAmistad + valor
+    }
+    this.atacar= function(pokemon){
+    	pokemon.vida = pokemon.vida - this.poderDeAtaque
+    }
+}
+const Pikachu = new Pokemon ("Pikachu","amarillo",100);
+const Charmander = new Pokemon ("charmander","rojo",20);
+
+Pikachu.atacar(Charmander);
+console.log(Charmander.vida)
+
