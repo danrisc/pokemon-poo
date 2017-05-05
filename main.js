0878f424e4470dd87b4fb01a15999137d82a3ae3
daniela risco <driscop@gmail.com>
2017-05-05 10:12:13 -0300

    avance ejercicio pokemon

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 function Pokemon(nombre,color,poderDeAtaque){
 	this.nombre= nombre;
 	this.color= color;
-	this.nivelDeAmistad = 0 
-	this.vida = 100
-	this.poderDeAtaque= poderDeAtaque
+	this.nivelDeAmistad = 0; 
+	this.vida = 100;
+	this.poderDeAtaque= poderDeAtaque;
 
 	this.mostrarPokemon = function(){
 		return("hola, soy: "+ this.nombre+ "y soy de color"+ this.color)
@@ -17,6 +17,8 @@ function Pokemon(nombre,color,poderDeAtaque){
 }
 const Pikachu = new Pokemon ("Pikachu","amarillo",100);
 const Charmander = new Pokemon ("charmander","rojo",20);
+const Rattata = new Pokemon ("Rattata","morado", 70);
+const Mewtwo = new Pokemon ("Mewtwo","violeta",130);
 
 Pikachu.atacar(Charmander);
 console.log(Charmander.vida)
